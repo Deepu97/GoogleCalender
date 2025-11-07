c97e61c244ab5d6ca20fd6003607d16f98ae8df8
Deepu97 <90163373+Deepu97@users.noreply.github.com>
2025-11-07 12:24:34 +0530

    Fix redirect_uri trailing slash in index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.post("/get-token", async (req, res) => {
       code,
       client_id: process.env.GOOGLE_CLIENT_ID,
       client_secret: process.env.GOOGLE_CLIENT_SECRET,
-      redirect_uri: "http://localhost:3000",
+      redirect_uri: "http://localhost:3000/",
       grant_type: "authorization_code",
     });
 
