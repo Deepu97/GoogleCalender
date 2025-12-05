7604a97f32fa38f77b554fd63954502f85e4fa56
Deepu97 <dy775829@gmail.com>
2025-12-05 14:03:15 +0530

    commonjs use

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-const express=require('express');
-require("dotenv").config();
-const cors=require('cors');
-const app=express();
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+import GoogleMeet from "./Routes/GoogleCalender/GoogleMeet.js";
+import GeminiRoute from "./Routes/Gemini/GeminiRoute.js";
+
+const app = express();
 app.use(cors());
 app.use(express.json());
-const GoogleMeet=require('./Routes/GoogleCalender/GoogleMeet');
-const GeminiRoute=require('./Routes/Gemini/GeminiRoute');
-app.use('/',GoogleMeet);
-app.use('/',GeminiRoute);
-app.listen(process.env.Port,()=>{
-    
-    console.log("server listen on port 5000");
-})
+
+app.use("/", GoogleMeet);
+app.use("/", GeminiRoute);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on port ${process.env.PORT}`);
+});
